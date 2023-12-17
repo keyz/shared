c79a5561e3628a8c4330104533d6b160a9cbb5e0
Keyan Zhang <2268452+keyz@users.noreply.github.com>
2023-12-17 00:59:02 -0800

    Release `stdlib` 0.11.0

---
+++ packages/stdlib/src/__tests__/types.test.ts
@@ -0,0 +1,24 @@
+import { test } from "vitest";
+import type { TAnyLiteralObject } from "../public/types";
+
+test("types: typedObjectHasOwn", () => {
+  onlyLiteralObject({});
+  onlyLiteralObject({ a: 3 });
+  onlyLiteralObject({ a: 3 } as const);
+
+  // @ts-expect-error
+  onlyLiteralObject([]);
+  // @ts-expect-error
+  onlyLiteralObject(null);
+
+  class Foo {
+    something = 123;
+  }
+
+  // @ts-expect-error
+  onlyLiteralObject(new Foo());
+
+  function onlyLiteralObject(x: TAnyLiteralObject) {
+    return x;
+  }
+});
